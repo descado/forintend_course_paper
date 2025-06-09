890ed2c37e2466a2b21758f4a999ad727dee95d9
descado <decado620@gmail.com>
2025-06-09 14:24:57 +0300

    del

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -7,8 +6,6 @@ import './index.css';
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <HashRouter>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </HashRouter>,
 );
